91f86f8ee9cc26c20acacf504d98d778a087f5ba
wangxueyan <wangxueyan@neotel.com.cn>
2020-04-07 19:38:48 +0800

    init

---
+++ client/client/client.cpp
@@ -0,0 +1,163 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include<winsock2.h>
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<PROCESS.H>
+#include <process.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h> 
+#include <cstdio>
+#include <iostream>
+#include <ws2tcpip.h> 
+#define PORT 5000
+
+using namespace std;
+int first = 1;
+//监视输入的线程函数
+void  getInput(void* param)
+{
+	SOCKET sock = (SOCKET)param;
+	while (1)
+	{
+		char buf[1024];
+		gets_s(buf);
+		int sendbyte = send(sock, buf, strlen(buf), 0);
+		if (sendbyte == SOCKET_ERROR)
+		{
+			printf("send() failed:%d\n", WSAGetLastError());
+		}
+	}
+}
+int main()
+{
+	char flag, flag1;
+	int loginmax = 3;
+	int connectmax = 5;
+	int i = 1;
+	int flag2;
+	int flag3 = 0;
+	time_t timenow;
+	WSADATA wsadata1;
+	char name[15] ;
+	char password[30];
+	flag1 = 'y';
+	//输口令
+	do {
+		cout<<"Please input the password:"<<endl;
+		gets_s(password);
+		if (strcmp(password, "123") == 0)
+			break;
+		else
+		{
+			loginmax -= 1;
+			if (loginmax)
+				cout<<"You have %d times to change the password!"<<loginmax<<endl;
+			else
+				printf("The system will be closed!\n");
+		}
+	} while (loginmax);
+	if (loginmax)
+	{
+		cout<<"Please input your name:"<<endl;
+		gets_s(name);
+		do {
+			cout<<"Do you want to establish a connection with the server?(y/n):"<<endl;
+			cin >> flag;
+			//建立连接
+			if (flag == 'y')
+			{
+				connectmax--;
+				i = 0;
+				int err = WSAStartup(WINSOCK_VERSION, &wsadata1);
+				if (err != 0)
+				{
+					return -1;
+				}
+				SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+				if (sock == INVALID_SOCKET)
+				{
+					WSACleanup();
+					return -1;
+				}
+				sockaddr_in serveraddress;
+				serveraddress.sin_family = AF_INET;
+				serveraddress.sin_port = htons(PORT);
+				string ip = "127.0.0.1";
+				inet_pton(AF_INET, ip.data(), (void*)&serveraddress.sin_addr.s_addr);
+//				serveraddress.sin_addr.S_un.S_addr = inet_pton("127.0.0.1");
+				do {
+					if (flag3)
+					{
+						connectmax--;
+						if (connectmax)
+							cout<<"connecting..."<<endl;
+						else
+						{
+							return -1;
+						}
+					}
+					flag3 = 1;
+					err = connect(sock, (sockaddr*)&serveraddress, sizeof(serveraddress));
+					if (err == INVALID_SOCKET)
+					{
+						getchar();
+						cout<<"If you want to try again,please press 'y':"<<endl;
+						cin >> flag1;
+						if (flag1 == 'y')
+							continue;
+						else
+						{
+							closesocket(sock);
+							WSACleanup();
+							return -1;
+						}
+					}
+					else
+						break;
+				} while (flag1 == 'y');
+				//连接成功
+				send(sock, name, strlen(name), 0);
+				flag2 = recv(sock, name, 100, 0);
+				if (flag2 == SOCKET_ERROR)
+				{
+					return -1;
+				}
+				system("cls");
+				cout<<"Welcome to the chatroom!\n"<<endl;
+				_beginthread(getInput, 0, (void*)sock);
+				Sleep(1000);
+				char rbuf[1024];
+				while (1)
+				{
+					memset(rbuf, 0, 1024);
+					int recbyte = recv(sock, rbuf, 1000, 0);
+					if (recbyte == SOCKET_ERROR)
+					{
+						closesocket(sock);
+						WSACleanup();
+						return -1;
+					}
+					timenow = time(NULL);
+					if (first)
+					{
+						first = 0;
+						continue;
+					}
+					printf("%s\n",rbuf);
+					Sleep(1000);
+				}
+				closesocket(sock);
+				WSACleanup();
+				return 0;
+			}
+			else if (flag == 'n')
+				return 0;
+			else
+			{
+				getchar();
+			}
+		} while (i);
+	}
+}
---
+++ server/server/server.cpp
@@ -0,0 +1,191 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include<winsock2.h>
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<iostream>
+#include <Ws2tcpip.h>
+#define MAX_NUM  50
+#define PORT 5000
+#define MSG_MAX_SIZE 1000
+
+
+using namespace std;
+
+int main()
+{
+	int sendbyte, i, j, m, people = 0;
+	char MesAll[MAX_NUM][MSG_MAX_SIZE + 25] ;
+	int first[MAX_NUM] = { 0 };
+	char name[MAX_NUM][25] = {0};
+	char buf[MAX_NUM][MSG_MAX_SIZE] ;
+	time_t   timenow;
+	WSADATA wsadata;
+	int err = WSAStartup(WINSOCK_VERSION, &wsadata);
+	if (err != 0)
+	{
+		return -1;
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		printf("socket() failed:%d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	sockaddr_in localaddress;
+	localaddress.sin_family = AF_INET;
+	localaddress.sin_port = htons(PORT);
+	localaddress.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+	err = bind(sock, (sockaddr*)&localaddress, sizeof(localaddress));
+	if (err == INVALID_SOCKET)
+	{
+		closesocket(sock);
+		WSACleanup();
+		return -1;
+	}
+
+	err = listen(sock, 6);
+	if (err == INVALID_SOCKET)
+	{
+		closesocket(sock);
+		WSACleanup();
+		return -1;
+	}
+	SOCKET client[MAX_NUM];
+	for (int i = 0; i < MAX_NUM; i++)
+	{
+		client[i] = INVALID_SOCKET;
+	}
+
+	fd_set rset, allset;
+	FD_ZERO(&allset);
+	FD_SET(sock, &allset);
+	while (1)
+	{
+		rset = allset;
+		int ret = select(0, &rset, NULL, NULL, NULL);
+		if (ret == SOCKET_ERROR)
+		{
+			break;
+		}
+
+
+		if (ret == 0) {
+			continue;
+		}
+
+		if (FD_ISSET(sock, &rset))
+		{
+			sockaddr_in clientaddr;
+			int len = sizeof(clientaddr);
+			SOCKET sockconn = accept(sock, (sockaddr*)&clientaddr, &len);
+			if (sockconn == INVALID_SOCKET)
+			{
+				break;
+			}
+			for (i = 0; i < MAX_NUM; i++)
+			{
+				if (client[i] == INVALID_SOCKET)
+				{
+					client[i] = sockconn;
+					break;
+				}
+			}
+			if (i < MAX_NUM)
+			{
+				FD_SET(sockconn, &allset);
+			}
+
+			else {
+				cout<<"too many client"<<endl;
+				closesocket(sockconn);
+			}
+
+		}
+		for (i = 0; i < MAX_NUM; i++)
+		{
+			if ((client[i] != INVALID_SOCKET) && FD_ISSET(client[i], &rset))
+			{
+				memset(buf[i], 0, MSG_MAX_SIZE);
+				int recbyte = recv(client[i], buf[i], MSG_MAX_SIZE, 0);//收到数据
+				if (recbyte == SOCKET_ERROR)
+				{
+					first[i] = 0;//若退出，则将客户信息置为初始值
+					people = 0;
+					for (m = 0; m < MAX_NUM; m++)
+						if (first[m] == 1)
+							people++;
+					printf("%s exit the chat room.目前在线人数：%d.\n", name[i], people);
+					strcpy_s(MesAll[i], "From server: ");
+					strcat_s(MesAll[i], name[i]);
+					strcat_s(MesAll[i], " 下线.");
+					FD_CLR(client[i], &allset);
+					closesocket(client[i]);
+					client[i] = INVALID_SOCKET;
+					//告诉其他客户有人下线
+					for (j = 0; j < MAX_NUM; j++)
+					{
+						if ((client[j] != INVALID_SOCKET))
+						{
+							sendbyte = send(client[j], MesAll[i], 1024, 0);
+							if (sendbyte == SOCKET_ERROR)
+							{
+								printf("send() failed:%d\n", WSAGetLastError());
+							}
+
+						}
+					}
+					continue;
+				}
+				//如果有新人进入，则给客户端发送提示消息
+				if (!first[i])
+				{
+					strcpy_s(MesAll[i], "From server: ");
+					strcat_s(MesAll[i], buf[i]);
+					strcat_s(MesAll[i], " 上线.");
+				}
+				else
+				{
+					strcpy_s(MesAll[i], name[i]);
+					strcat_s(MesAll[i], ": ");
+					strcat_s(MesAll[i], buf[i]);
+				}
+				for (j = 0; j < MAX_NUM; j++)
+				{
+					if ((client[j] != INVALID_SOCKET))
+					{
+						sendbyte = send(client[j], MesAll[i], 1000, 0);
+						if (sendbyte == SOCKET_ERROR)
+						{
+							printf("send() failed:%d\n", WSAGetLastError());
+						}
+
+					}
+				}
+				time(&timenow);
+				//服务器端显示新进客户的信息
+				if (!first[i])
+				{
+					people = 0;
+					for (m = 0; m < MAX_NUM; m++)
+						if (first[m] == 1)
+							people++;
+					strcpy_s(name[i], buf[i]);
+					printf("目前在线人数：%d\n", people + 1);
+					first[i] = 1;
+				}
+				else
+					cout << name[i] << buf[i] << endl;
+
+			}
+		}
+
+	}
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
